c91cabdb159f3e8276a1b718eaad13e454f872e1
Ayoub Malek <ga54buj@tum.de>
2017-10-15 02:12:50 +0200

    adding the main script

---
+++ main.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 23 19:05:35 2017
+@author: SuperKogito
+"""
+from Gui import MainWindow
+
+
+def main():
+    MainWindow('Port scanner')
+
+if __name__ == '__main__':
+    main()
